97033e533175eae6e9b9e0812fc936318bf2cce8
noname <baev_aleksei@mail.ru>
2021-06-10 12:45:16 +0300

    url argument changed to required

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def check_bitlink(user_url, headers):
 def create_argument_parser():
     parser = argparse.ArgumentParser(description="""Getting short links 
                                     using the Bitly service API.""")
-    parser.add_argument('-url', help='Sends url for execution')
+    parser.add_argument('url', help='Sends url for execution')
     return parser
 
 
